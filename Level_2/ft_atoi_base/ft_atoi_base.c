202eeeba6e8e9c1b813ae4be1b0188e9d269d033
Juan Carlos Alcausa Luque <jcalcausal@IMac-jcalcausa-2.local>
2024-11-25 15:22:03 +0100

    ft_atoi_base

---
+++ Level_2/ft_atoi_base/ft_atoi_base.c
@@ -0,0 +1,40 @@
+int	ft_atoi_base(const char *str, int str_base)
+{
+	int i = 0;
+	int sign = 1;
+	int res = 0;
+	char c = 0;
+	
+	if (str[i] == '-')
+	{
+		sign = -1;
+		++i;
+	}
+	while (str[i])
+	{
+		c = str[i];
+		if (c >= 'A' && c <= 'Z')
+			c = c + 32;
+		if (c >= '0' && c <= '9')
+			c = c - '0';
+		else if (c >= 'a' && c <= 'z')
+			c = c - 'a' + 10;
+		else
+			return (0);
+		if (c >= str_base)
+			return (0);
+		res = res * str_base + c;
+		++i;
+	}
+	return (res * sign);
+}
+
+#include <stdio.h>
+int main()
+{
+	const char *str = "24";
+	int str_base = 5;
+	int s = ft_atoi_base(str, str_base);
+	printf("%d\n", s);
+	return (0);
+}
