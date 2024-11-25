f6ba66f02cc8a8e59686790f552810e3e52c5b99
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2024-11-25 18:36:35 +0100

    ft_atoi_base

--- Level_2/ft_atoi_base/ft_atoi_base.c
+++ Level_2/ft_atoi_base/ft_atoi_base.c
@@ -13,11 +13,11 @@ int	ft_atoi_base(const char *str, int str_base)
 	while (str[i])
 	{
 		c = str[i];
-		if (c >= 'A' && c <= 'Z')
+		if (c >= 'A' && c <= 'F')
 			c = c + 32;
 		if (c >= '0' && c <= '9')
 			c = c - '0';
-		else if (c >= 'a' && c <= 'z')
+		else if (c >= 'a' && c <= 'f')
 			c = c - 'a' + 10;
 		else
 			return (0);
@@ -36,6 +36,9 @@ int main()
 	int str_base = 5;
 	int s = ft_atoi_base(str, str_base);
 	printf("%d\n", s);
+	printf("%d\n", ft_atoi_base("101", 2)); // 5 en decimal
+	printf("%d\n", ft_atoi_base("A", 16));  // 10 en decimal
+	printf("%d\n", ft_atoi_base("24", 5));  // 14 en decimal
 	return (0);
 }
 */
